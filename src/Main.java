01337a5746a769174848a0c36077af8c6413d12f
Matheus <entry_2015@mcarvalhor.com>
2018-05-22 23:15:57 -0300

    Adicionado projeto no repositorio.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * Classe principal do programa.
+ *
+ */
+public class Main {
+
+	/**
+	 * Função principal do programa.
+	 *
+	 * @param args argumentos de linha de comando
+	 */
+	public static void main(String[] args){
+		// A implementar...
+	}
+
+}
