e8f56884a6bb2303caca4cce58f0275656255d70
Matheus <entry_2015@mcarvalhor.com>
2018-06-11 00:58:14 -0300

    Atualizado com StartWindow.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Classe principal do programa.
@@ -10,8 +12,16 @@ public class Main {
 	 *
 	 * @param args argumentos de linha de comando
 	 */
-	public static void main(String[] args){
-		// A implementar...
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame MainWindow = new StartWindow();
+				MainWindow.setSize(400, 400);
+				MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				MainWindow.setVisible(true); // NÃO FUNCIONA
+			}
+		});
 	}
 
 }
